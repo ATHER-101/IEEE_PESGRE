8214ca794914d229a17c6a5f58b054499a763777
Satish Naik <68959100+Satishiitdh@users.noreply.github.com>
2024-12-26 18:56:25 +0100

    Update CardList.jsx

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -7,18 +7,18 @@ function CardList() {
         Organisers
       </h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {Array.from({ length: 1 }).map((_, idx) => (
+        {Array.from({ length: 4 }).map((_, idx) => (
           <div
             key={idx}
             className=" w-full bg-white rounded-lg border border-gray-200 shadow-lg transition-transform transform hover:-translate-y-1 hover:shadow-xl p-6 text-center"
           >
             <img className="mx-auto" src="/iitdh.png" alt="iitdh logo" />
-            <img className="mx-auto" src="/IEEE.png" alt="iitdh logo" />
-            <img className="mx-auto" src="/IEEEK.png" alt="iitdh logo" />
-            <img className="mx-auto" src="/IEEE-IAS-Logo.png" alt="iitdh logo" />
-            <img className="mx-auto" src="/ies-logo.png" alt="iitdh logo" />
-             <img className="mx-auto" src="/PELS-Logo.png" alt="iitdh logo" />
-             <img className="mx-auto" src="/PES-Logo.png" alt="iitdh logo" />
+           <! -- <img className="mx-auto" src="/IEEE.png" alt="iitdh logo" /> -->
+           <! --  <img className="mx-auto" src="/IEEEK.png" alt="iitdh logo" /> -->
+          <! --  <img className="mx-auto" src="/IEEE-IAS-Logo.png" alt="iitdh logo" /> -->
+          <! --   <img className="mx-auto" src="/ies-logo.png" alt="iitdh logo" /> -->
+            <! --  <img className="mx-auto" src="/PELS-Logo.png" alt="iitdh logo" /> -->
+            <! --  <img className="mx-auto" src="/PES-Logo.png" alt="iitdh logo" /> -->
           </div>
         ))}
       </div>
