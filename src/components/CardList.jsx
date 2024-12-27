a8ba5dd729534b76e6e76510604142b904d1462d
ATHER-101 <atharvatijare04@gmail.com>
2024-12-27 13:37:57 +0530

    cards.jsx updated

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 
+const cards = [
+  {
+    "src":"/IEEE.png",
+    "alt":"IEEE logo"
+  },
+  {
+    "src":"/IEEEK.png",
+    "alt":"IEEE Kerala logo"
+  },
+  {
+    "src":"/IEEE-IAS-Logo.png",
+    "alt":"IEEE IAS logo"
+  },
+  {
+    "src":"/ies-logo.png",
+    "alt":"IEEE IES logo"
+  },
+  {
+    "src":"/PELS-Logo.png",
+    "alt":"PELS logo"
+  },
+  {
+    "src":"/PES-Logo.png",
+    "alt":"PES logo"
+  },
+]
+
 function CardList() {
   return (
     <div className="w-full bg-gray-100 p-6">
       <h3 className="text-center text-blue-600 text-3xl pb-5 pl-6">
         Organisers
       </h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {Array.from({ length: 4 }).map((_, idx) => (
+      <div className="grid grid-cols-2 lg:grid-cols-6 gap-2">
+        {cards.map((card, idx) => (
           <div
             key={idx}
-            className=" w-full bg-white rounded-lg border border-gray-200 shadow-lg transition-transform transform hover:-translate-y-1 hover:shadow-xl p-6 text-center"
+            className=" w-full bg-white rounded-lg border border-gray-200 shadow-lg transition-transform transform hover:-translate-y-1 hover:shadow-xl p-6 text-center flex items-center justify-center"
           >
-            <img className="mx-auto" src="/iitdh.png" alt="iitdh logo" />
-            
+            <img src={card.src} alt={card.alt} />
+
           </div>
         ))}
       </div>
