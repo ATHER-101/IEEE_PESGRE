fbb2d6bcc3103ca7a0cf1c6da6efb451220e968b
ATHER-101 <atharvatijare04@gmail.com>
2025-04-01 15:47:15 +0530

    Auto-deploy: 2025-04-01 15:47:15

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic Route
+app.get("/", (req, res) => {
+  res.send({"response":"Hello, Express Backend!"});
+});
+
+// Start the server
+const PORT = 4000;
+app.listen(PORT, "localhost",() => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
